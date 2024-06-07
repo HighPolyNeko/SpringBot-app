3a3ba97d2890363ca6edfd75e3090f2ef6d61edc
Nabokov Nikolay <n.nabokov1109@gmail.com>
2024-06-07 23:21:28 +0300

    init

---
+++ src/main/java/ru/netology/springapp/config/JavaConfig.java
@@ -0,0 +1,27 @@
+package ru.netology.springapp.config;
+
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.springapp.profile.DevProfile;
+import ru.netology.springapp.profile.ProductionProfile;
+import ru.netology.springapp.profile.SystemProfile;
+
+@Setter
+@Configuration
+
+public class JavaConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
